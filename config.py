82f832064af93dceab56b0ef1ec8d2516d887efe
Bhadri Narayanan <66205567+bhadrii@users.noreply.github.com>
2020-08-02 00:02:58 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,53 @@
+
+from ntk.tokenize import word_tokenize
+from ntk.porter import PorterStemmer
+#create an object of class PorterStemmer
+porter = PorterStemmer()
+SETUP_PATH="masterdata.json"
+CUTOFF_FREQ=5
+URL='https://covidwire.firebaseio.com/corpus.json'
+
+
+stopwords=['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+
+def stemtokenize(document):
+    """
+    Given a document (represented as a string), return a list of all of the
+    words in that document, in order.
+
+    Process document by coverting all words to lowercase, and removing any
+    punctuation or English stopwords WITH STEMMING.
+    """
+    tokens=[]
+    sentence=word_tokenize(document)
+
+    for word in sentence:
+        lowered=word.lower()
+        for letter in word:
+            if not letter.isalnum():
+                lowered=lowered.replace(letter,"")
+        if lowered not in stopwords and lowered != '' and not lowered.isnumeric():
+            tokens.append(porter.stem(lowered))
+    return tokens
+
+def tokenize(document):
+    """
+    Given a document (represented as a string), return a list of all of the
+    words in that document, in order.
+
+    Process document by coverting all words to lowercase, and removing any
+    punctuation or English stopwords WITH STEMMING.
+    """
+    tokens=[]
+    sentence=word_tokenize(document)
+
+    for word in sentence:
+        lowered=word.lower()
+        for letter in word:
+            if not letter.isalnum():
+                lowered=lowered.replace(letter,"")
+        if lowered not in stopwords and lowered != '' and not lowered.isnumeric():
+            tokens.append(lowered)
+    return tokens
+
+
